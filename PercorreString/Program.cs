542830b0c9e6da6317b0ed7447b7154f2f3f03a4
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-09 21:11:25 +0000

    Exercicio feito

---
+++ PercorreString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva uma frase: ");
+            string frase = Console.ReadLine();
+
+            Console.WriteLine("Escolha uma letra que esteja na frase pelo menos: ");
+            string letra = Console.ReadLine();
+
+            int verify = Convert.ToInt32(letra[0]);
+
+            for( int i = 0; i < frase.Length;i++ )
+            {
+                int letraFrase = Convert.ToInt32(frase[i]);
+                if(verify != frase[i])
+                {
+                    Console.Write(frase[i]);
+                }
+            }
+
+        }
+    }
+}
