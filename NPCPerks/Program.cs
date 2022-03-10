ab5ff58ca4cce1612035b55613c3b04d44926f32
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-10 08:26:05 +0000

    Numero de NPCS  e ciclo

---
+++ NPCPerks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NPCPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerosNPCs;
+            int [] classes;
+            
+
+            Console.WriteLine("Quantos camaradas existem: ");
+            Console.ReadLine(numerosNPCs);
+            
+            classes = new int[numerosNPCs];
+
+            for (int i = 0; i < classes[i]; i++)
+            {
+                Console.WriteLine("Escolhe a/s classes do NPC: ");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
